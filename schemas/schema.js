9d0b26325144e766e8ab23cd3619868410f9cd58
Philip Laxamana <philip.j.laxamana@gmail.com>
2022-06-21 13:26:07 -0400

    change

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,4 +1,4 @@
-// First, we must import the schema creator
+// First, we must import the schema creator!
 import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
